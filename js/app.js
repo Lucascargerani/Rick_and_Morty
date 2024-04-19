43aa27a5366607aae917e9fe325e36076b684a81
Lucas Cargerani <lucascargerani@gmail.com>
2024-04-18 23:51:40 -0300

    Adicionando lógica do fetch

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 axios.get('https://rickandmortyapi.com/api/character')
   .then(response => {
     const characters = response.data.results;
-    const charactersListDiv = document.getElementById('characters-list');
+    const charactersListDiv = document.getElementById('divPrincipal');
 
     characters.forEach(character => {
-      const characterName = document.createElement('p');
-      characterName.textContent = character.name;
-      charactersListDiv.appendChild(characterName);
+      const characterDiv = document.createElement('div');
+      characterDiv.classList.add('charactersDiv');
+
+      const namePerson = createElement('p');
+      namePerson.textContent = character.name;
+      characterDiv.append(namePerson);
+      charactersListDiv.appendChild(characterDiv);
     });
   })
   .catch(error => {
