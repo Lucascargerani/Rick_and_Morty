62a72dde21464a45153623343ac12c1e9a4549c7
Lucas Cargerani <lucascargerani@gmail.com>
2024-04-20 17:14:42 -0300

    Co-authored-by: IgorEverton <IgorEverton@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -4,13 +4,31 @@ axios.get('https://rickandmortyapi.com/api/character')
     const charactersListDiv = document.getElementById('divPrincipal');
 
     characters.forEach(character => {
-      const characterDiv = document.createElement('div');
-      characterDiv.classList.add('charactersDiv');
 
-      const namePerson = createElement('p');
+      const status = document.createElement('p');
+      const namePerson = document.createElement('p');
+      const especie = document.createElement('p');
+      const img = document.createElement('img')
+
+      img.src = character.image;
+      especie.textContent = character.species;
+      status.textContent = character.status;
       namePerson.textContent = character.name;
-      characterDiv.append(namePerson);
+      const characterDiv = document.createElement('div');
+
+
+
+      characterDiv.classList.add('charactersDiv');
+      namePerson.classList.add('infoDiv');
+      img.classList.add('iconPersons');
+      especie.classList.add('especie');
+      characterDiv.appendChild(img);
+      characterDiv.appendChild(namePerson);
+      characterDiv.appendChild(especie);
+
       charactersListDiv.appendChild(characterDiv);
+
+
     });
   })
   .catch(error => {
