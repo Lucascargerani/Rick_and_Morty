4874bbfb71500514902afb36bb8a24de2315b03c
Lucas Cargerani <lucascargerani@gmail.com>
2024-04-26 14:42:48 -0300

    Co-authored-by: IgorEverton <IgorEverton@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,36 +1,48 @@
-axios.get('https://rickandmortyapi.com/api/character')
+var pageIndex = 2;
+
+axios.get('https://rickandmortyapi.com/api/character?limit=8',{
+  params:{
+    page: pageIndex
+  }
+})
   .then(response => {
     const characters = response.data.results;
     const charactersListDiv = document.getElementById('divPrincipal');
 
-    characters.forEach(character => {
-
-      const status = document.createElement('p');
-      const namePerson = document.createElement('p');
-      const especie = document.createElement('p');
-      const img = document.createElement('img')
-
-      img.src = character.image;
-      especie.textContent = character.species;
-      status.textContent = character.status;
-      namePerson.textContent = character.name;
-      const characterDiv = document.createElement('div');
 
 
 
-      characterDiv.classList.add('charactersDiv');
-      namePerson.classList.add('infoDiv');
-      img.classList.add('iconPersons');
-      especie.classList.add('especie');
-      characterDiv.appendChild(img);
-      characterDiv.appendChild(namePerson);
-      characterDiv.appendChild(especie);
 
-      charactersListDiv.appendChild(characterDiv);
+    // let i = 0;
+    // let count = 8;
+    // while(i< count) {
+
+    //   const status = document.createElement('p');
+    //   const namePerson = document.createElement('p');
+    //   const especie = document.createElement('p');
+    //   const img = document.createElement('img')
+
+    //   img.src = characters[i].image;
+    //   especie.textContent = characters[i].species;
+    //   status.textContent = characters[i].status;
+
+    //   namePerson.textContent = characters[i].name;
+    //   const characterDiv = document.createElement('div');
 
 
-    });
-  })
-  .catch(error => {
+
+    //   characterDiv.classList.add('charactersDiv');
+    //   namePerson.classList.add('infoDiv');
+    //   img.classList.add('iconPersons');
+    //   especie.classList.add('especie');
+    //   characterDiv.appendChild(img);
+    //   characterDiv.appendChild(namePerson);
+    //   characterDiv.appendChild(especie);
+
+    //   charactersListDiv.appendChild(characterDiv);
+
+    //   i++;
+    // }})
+  .catch (error => {
     console.error('Ocorreu um erro ao fazer a requisição:', error);
   });
