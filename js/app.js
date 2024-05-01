848526a3785932e84819ec464748e810f216c997
Lucas Cargerani <lucascargerani@gmail.com>
2024-04-30 23:12:08 -0300

    Select

--- js/app.js
+++ js/app.js
@@ -6,28 +6,52 @@ axios.get('https://rickandmortyapi.com/api/character?limit=8',{
 })
 .then(response =>{
     const characters = response.data.results;
-    const charactersListDiv = document.getElementById('divPrincipal');
+    const charactersListDiv = document.getElementById('charactersList');
 
     const specie = document.getElementById('select_Species');
     specie.innerHTML = ''
 
-    const status = document.createElement('p');
-    const namePerson = document.createElement('p');
-    const especie = document.createElement('p');
-    const img = document.createElement('img')
+    const gender = document.getElementById('select_Gender');
+    specie.innerHTML = ''
 
-    characters.forEach(characters => {
-      const option = document.createElement('option');
-      option.textContent = characters.species;
-      specie.appendChild(option)
-      console.log("especie: ",characters.specie)
+    const statu = document.getElementById('select_Status');
+    specie.innerHTML = ''
+    // const specieOption = characters.map(characters => characters.specie);
+    // const uniqueOption = specieOption.filter((specie, index) => specie && specie.indexOf(specie === index));
+
+    //criar uma variavel tipo array
+    //adicionar as specie no array 
+    //criar outro array com o metodo Set
+
+
+    let specieOption = characters.species
+    const newSpecie = [... new Set(specieOption)];
+
+
+    newSpecie.forEach(newSpecie => {
+      const optionSpecie = document.createElement('option');
+      optionSpecie.textContent = newSpecie
+      specie.appendChild(optionSpecie);
     });
+      // const optionGender = document.createElement('option');
+      // optionGender.textContent = characters.gender;
+      // gender.appendChild(optionGender);
+
+      // const optionStatus = document.createElement('option');
+      // optionStatus.textContent = characters.status;
+      // statu.appendChild(optionStatus);
+
+
+    
 
     let i = 0;
     let count = 8;
-    while(i< count) {
+    while(i < count) {
 
-
+    const status = document.createElement('p');
+    const namePerson = document.createElement('p');
+    const especie = document.createElement('p');
+    const img = document.createElement('img');
 
     img.src = characters[i].image;
     especie.textContent = characters[i].species;
@@ -49,6 +73,3 @@ axios.get('https://rickandmortyapi.com/api/character?limit=8',{
     i++;
      }
     })
-    .catch((error)=>{
-      console.log(error)
-  })
